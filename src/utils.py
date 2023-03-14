eede7a65ae9f6243e53d2462642e66e6f8ed3b4e
Prahtz <lorenzopratesi3@gmail.com>
2023-03-14 21:27:51 +0100

    Add distributed utils

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+import os
 import math
 import torch
 import random
@@ -86,3 +87,43 @@ class DistributedMultiplexerSampler(DistributedSampler):
 
     def __len__(self) -> int:
         return self.num_samples
+
+def set_seed(seed: int):
+    """
+    Initializes the random seed.
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+def gather_tensor(tensor, rank, num_replicas, dst_rank):
+    """
+    Wraps the distributed gather function by automatically creating the tensor list.
+    """
+    tensor_list = [tensor for _ in range(num_replicas)] if rank == dst_rank else None
+    dist.gather(tensor, tensor_list, dst=0)
+    return tensor_list
+
+def init_distributed():
+    """
+    Initializes the distributed environment.
+    """
+    if dist.is_torchelastic_launched():
+        backend = 'gloo'
+        if torch.cuda.is_available():
+            backend = 'nccl'
+        dist.init_process_group(backend)
+        local_rank = int(os.environ['LOCAL_RANK'])
+        
+        if torch.cuda.is_available():
+            torch.cuda.set_device(local_rank)
+
+def get_ranks_and_replicas():
+    """
+    Returns the local rank, the global rank and the number of running processes.
+    """
+    local_rank, global_rank, num_replicas = 0, 0, 1
+    if dist.is_torchelastic_launched():
+        local_rank, global_rank, num_replicas = int(os.environ['LOCAL_RANK']), int(os.environ['RANK']), dist.get_world_size()
+    return local_rank, global_rank, num_replicas

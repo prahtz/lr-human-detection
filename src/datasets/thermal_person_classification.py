3cf863c314e2f56f9ada7a2cdcacbdcc4075dff0
Prahtz <lorenzopratesi3@gmail.com>
2023-03-19 18:15:21 +0100

    Edit data loading, optimizing it

--- src/datasets/thermal_person_classification.py
+++ src/datasets/thermal_person_classification.py
@@ -4,44 +4,46 @@ import random
 import torch
 
 from torch.utils.data import Dataset
-from typing import List, Tuple
+from typing import List
 
 class Positives(Dataset):
     def __init__(self, root_path: str, split: str = 'train') -> None:
         self.root_path = root_path
         self.split = split
-        with h5py.File(self.root_path, 'r') as dataset_file:
-            self.len = len(dataset_file[split])   
+        self.dataset_file = h5py.File(self.root_path, 'r')
         
     def __len__(self) -> int:
-        return self.len
+        return len(self.dataset_file[self.split]) 
     
     def __getitem__(self, idx: int):
-        with h5py.File(self.root_path, 'r') as dataset_file:
-            split_group = dataset_file[self.split]
-            image = split_group[f'img_{idx}'][:]
-            return torch.from_numpy(image), torch.scalar_tensor(1).long()
+        split_group = self.dataset_file[self.split]
+        image = split_group[f'img_{idx}'][:]
+        return torch.from_numpy(image), torch.scalar_tensor(1).long()
+    
+    def __del__(self):
+        self.dataset_file.close()
 
 class Negatives(Dataset):
     def __init__(self, root_path: str, split: str = 'train') -> None:
         self.root_path = root_path
         self.split = split
-        with h5py.File(self.root_path, 'r') as dataset_file:
-            self.len = len(dataset_file[split]['free_points'])
+        self.dataset_file = h5py.File(self.root_path, 'r')
         
     def __len__(self) -> int:
-        return self.len
+        return len(self.dataset_file[self.split]['free_points'])
     
     def __getitem__(self, idx: int):
-        with h5py.File(self.root_path, 'r') as dataset_file:
-            split_group = dataset_file[self.split]
-            item = split_group['free_points'][f'item_{idx}']
-            rand_idx = random.randint(0, item.shape[0] - 1)
-            y, x = item[rand_idx]
-            box_size, image_path = item.attrs['box_size'], item.attrs['image_path']
-            image = split_group[f'images/{image_path}'][:, y:y+box_size, x:x+box_size]
-            return torch.from_numpy(image), torch.scalar_tensor(0).long()
-
+        split_group = self.dataset_file[self.split]
+        item = split_group['free_points'][f'item_{idx}']
+        rand_idx = random.randint(0, item.shape[0] - 1)
+        y, x = item[rand_idx]
+        box_size, image_path = item.attrs['box_size'], item.attrs['image_path']
+        image = split_group[f'images/{image_path}'][:, y:y+box_size, x:x+box_size]
+        return torch.from_numpy(image), torch.scalar_tensor(0).long()
+    
+    def __del__(self):
+        self.dataset_file.close()
+        
 class ConcatDataset(Dataset):
     def __init__(self, data_sources: List[Dataset]):
         self.data_sources = data_sources
